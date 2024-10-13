374f48da2a9f23918946dadff6e13d3441cec616
Dhruv07Maurya <2022.dhruv.maurya@ves.ac.in>
2024-10-13 05:35:30 -0700

    added map in order page minor updates

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../components/Navbar";
 
 function NoPage() {
   return (
-    <div className='bg-gray-100 min-h-screen pl-60 pt-12'>
-        <img src="https://i0.wp.com/learn.onemonth.com/wp-content/uploads/2017/08/1-10.png?fit=845%2C503&ssl=1" alt="" />
-    </div>
-  )
+    <>
+      <Navbar />
+      <center><h1 className="text-3xl text-red-700">Please login with admin Credentials</h1></center>
+      <div className="bg-gray-100 min-h-screen pl-60 pt-12">
+        <img
+          src="https://i0.wp.com/learn.onemonth.com/wp-content/uploads/2017/08/1-10.png?fit=845%2C503&ssl=1"
+          alt=""
+        />
+      </div>
+    </>
+  );
 }
 
-export default NoPage
+export default NoPage;
